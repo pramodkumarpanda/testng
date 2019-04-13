5c971bf9f7e59215a0d1ddd7c738cb33b2f94763
root <root@server4.example.com>
2019-04-13 09:56:59 +0530

    Testng analysis

---
+++ catlog/src/test/java/com/a/b/c/AppTest.java
@@ -0,0 +1,22 @@
+package com.a.b.c;
+
+//import static org.junit.Assert.assertTrue;
+//import org.testng.Test;
+import org.testng.annotations.Test;
+ 
+//import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    
+    @Test
+    public void unitLevel1() {
+        System.out.println("testClass: Unit level1 testing");
+    }
+}
